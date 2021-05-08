0a74ffa300274ac39bbeb210d60a108d59b76f6e
Samar Imam <71113446+samarimam@users.noreply.github.com>
2021-05-08 15:52:15 +0530

    Update Footer.tsx

--- components/atoms/Footer.tsx
+++ components/atoms/Footer.tsx
@@ -4,10 +4,10 @@ import InstagramIcon from 'components/icons/InstagramIcon';
 const Footer: React.FC = () => {
     return (
         <div className='p-4 font-display bg-gray-900 text-white text-sm container mx-auto text-center '>
-            <div className= " divider">
+            <div className= 'divider'>
                 <div>Follow us
-            <ul className="p-4">
-            <a className="p-4" href='https://www.linkedin.com/company/hashesjmi/about/' target='_blank' rel='noreferrer'>
+            <ul className='p-4'>
+            <a className='p-4' href='https://www.linkedin.com/company/hashesjmi/about/' target='_blank' rel='noreferrer'>
                 <LinkedinIcon className='h-6' />
             </a>
             <a href='https://www.instagram.com/hashes_jmi/' target='_blank' rel='noreferrer'>
